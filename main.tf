434132869a99cae1a4d602db179ec756e5bc1749
SantiagoNieto887883 <sant887883@gmail.com>
2025-06-03 11:23:05 -0500

    add user data

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 ## provider ##
 
 provider "aws" {
-region     = "us-east-1"
+  region     = "us-east-1"
   access_key = "AKIAQHJYVYOTMO4J6GDP"
   secret_key = "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"
 
@@ -15,9 +15,15 @@ locals {
 ## resource ##
 
 resource "aws_instance" "test_terraform" {
-  ami           = "ami-0440d3b780d96b29d"
+  #Recursos principales
+  #AMI que desplegará
+  ami = "ami-0440d3b780d96b29d"
+  #Tipo de instancia
   instance_type = "t2.micro"
+  #ID de la subnet
   subnet_id = "subnet-002b64e875dc8ce87"
+
+  #Etiquetas de los recursos
   tags = {
-    Name = "EC2-TEST-TERRAFORM" }
+  Name = "EC2-TEST-TERRAFORM" }
 }
