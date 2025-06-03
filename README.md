244e8855329c4813f4d7cd73f8deca5f0d73c50f
SantiagoNieto887883 <sant887883@gmail.com>
2025-06-03 11:28:32 -0500

    add comentarios

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Terraform_v3
+
+# Para cada Recurso se debe definir el json, ejemplo uno para la intancia y otro para una llave publica y otro para un grupo de seguridad
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 ## provider ##
 
 provider "aws" {
-  region     = "us-east-1"
+region     = "us-east-1"
   access_key = "AKIAQHJYVYOTMO4J6GDP"
   secret_key = "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"
 
@@ -17,13 +17,22 @@ locals {
 resource "aws_instance" "test_terraform" {
   #Recursos principales
   #AMI que desplegará
-  ami = "ami-0440d3b780d96b29d"
+  ami           = "ami-0440d3b780d96b29d"
   #Tipo de instancia
   instance_type = "t2.micro"
   #ID de la subnet
   subnet_id = "subnet-002b64e875dc8ce87"
-
+  
   #Etiquetas de los recursos
   tags = {
-  Name = "EC2-TEST-TERRAFORM" }
+    Name = "EC2-TEST-TERRAFORM" }
+
+
+    #Datos de USER DATA (se usan para instalar al inicio de un server)
+    user_data = <<-EOF
+             #!/bin/bash
+            sudo yum install -y nginx
+            sudo systemctl enable nginx
+            sudo systemctl start nginx
+            EOF
 }
